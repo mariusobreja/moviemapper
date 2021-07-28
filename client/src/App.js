834bc275c1d4ad4c56ab1e2728632b315a426d9e
Seb Green <sebastian_green@live.com>
2021-07-28 17:02:09 +0100

    added pin icon at hardcoded location, minor styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { Room } from '@material-ui/icons';
 import './App.css';
 
 function App() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: '90vw',
+    height: '80vh',
+    latitude: 51.3781,
+    longitude: -2.4360,
+    zoom: 4
   });
 
   return (
@@ -17,7 +18,17 @@ function App() {
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    >
+      <Marker 
+        latitude={51.4701} 
+        longitude={-0.9700} 
+        offsetLeft={-20} 
+        offsetTop={-10}
+      >
+      <Room style={{fontSize:viewport.zoom * 6,
+                    color: 'lightcoral'}}/>
+      </Marker>
+      </ReactMapGL>
     </div>
   );
 }
