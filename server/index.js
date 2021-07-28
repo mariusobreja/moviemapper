d722a6225b004c23a64e43ac1ae1e73e069d23ae
Seb Green <sebastian_green@live.com>
2021-07-28 11:17:39 +0100

    created and connected mongoose db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+const DB_NAME = process.env.DB_NAME;
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log('Server is running!🚀')
+app.use(cors());
+
+mongoose.connect(`${DB_URL}${DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('database connected!📍')
+  })
+  .catch((e) => console.log(e));
+
+app.listen(PORT, (e) => {
+  if (e) console.log(e);
+  console.log(`Server is running on http://localhost:${PORT}!!🚀`)
 })
 
