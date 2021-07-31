c79a5b1756e3253727d24015818bbf896741b2b1
Seb Green <sebastian_green@live.com>
2021-07-31 09:32:19 +0100

    fixed button css, stays in place on window change

---
+++ client/src/components/popup/popup.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import './popup.css';
+// import { Popup } from 'react-map-gl';
+// import { Star } from '@material-ui/icons';
+
+// export default function getPopup() {
+
+// }
