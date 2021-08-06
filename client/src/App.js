660055adcbbd2fcf0307eba3efd18687038a356b
Marius Obreja <marius.obreja9@gmail.com>
2021-08-06 11:08:53 +0100

    feat: add environmental vars

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,10 @@ function App() {
     width: '85vw',
     height: '80vh',
     latitude: 43.3781,
-    longitude: -44.4360,
+    longitude: -44.436,
     zoom: 2.7
   });
-  
+
   const [currentPinId, setCurrentPinId] = useState(null);
   const myStorage = window.localStorage;
   const [currentUser, setCurrentUser] = useState(myStorage.getItem('user'));
@@ -32,17 +32,17 @@ function App() {
       const res = await axios.get(process.env.REACT_APP_API);
       setPins(res.data);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
   const handlePopupClick = (id, lat, long) => {
     setCurrentPinId(id);
-    setViewport({...viewport, latitude: lat, longitude: long})
+    setViewport({ ...viewport, latitude: lat, longitude: long });
   };
 
   const handlePinClick = (e) => {
-    const [longitude,latitude] = e.lngLat;
+    const [longitude, latitude] = e.lngLat;
     setNewPin({
       latitude,
       longitude
@@ -57,104 +57,136 @@ function App() {
       description: movie,
       rating,
       latitude: newPin.latitude,
-      longitude: newPin.longitude,
+      longitude: newPin.longitude
     };
     try {
       const res = await axios.post(process.env.REACT_APP_API, newEntry);
       setPins([...pins, res.data]);
       setNewPin(null);
     } catch (e) {
-      console.log(e, 'error here')
+      console.log(e, 'error here');
     }
-  }
+  };
 
   const handleLogout = () => {
     myStorage.removeItem('user');
     setCurrentUser(null);
-  }
+  };
 
   useEffect(() => {
-    getAllPins()
-  },[])
+    getAllPins();
+  }, []);
 
   return (
-    <div className="App">
-      <h1 className='movieMapper'>Movie Mapper<Room className='titleLogo' style={{fontSize:36.2}}></Room></h1>
-      {currentUser ? (<button className='button logout' onClick={handleLogout}>Logout</button>) 
-      : (<div className='buttons'>
-        <button className='button login' onClick={()=>setShowLogin(true)}>Login</button>
-        <div className="divider"/>
-        <button className='button register' onClick={()=>setShowRegister(true)}>Register</button>
-        </div>)}
-        {showRegister && <Register setShowRegister={setShowRegister}/>}
-        {showLogin && <Login 
-        setShowLogin={setShowLogin} 
-        myStorage={myStorage}
-        setCurrentUser={setCurrentUser}/>}
-      <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapStyle='mapbox://styles/sebastiangreen/ckrnp8ur54xux17mswwup4dhk'
-      onDblClick={handlePinClick}
-    > 
-
-    {pins.map(pin => (
-    <>
-    <Marker
-    latitude={pin.latitude} 
-    longitude={pin.longitude} 
-    offsetLeft={-viewport.zoom * 4.25} 
-    offsetTop={-viewport.zoom * 8.5}
-    >
-    <Room style={{fontSize:viewport.zoom * 8.5,
-                  color: pin.username === currentUser ? 'lightcoral' : '#8b95c9',
-                  cursor: 'pointer'}}
-                  onClick={() => handlePopupClick(pin._id, pin.latitude, pin.longitude)}/>
-    </Marker>
-    {pin._id === currentPinId && (
-      <Popup
-          latitude={pin.latitude}
-          longitude={pin.longitude}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="bottom"
-          onClose={()=>setCurrentPinId(null)}
+    <div className='App'>
+      <h1 className='movieMapper'>
+        Movie Mapper
+        <Room className='titleLogo' style={{ fontSize: 36.2 }}></Room>
+      </h1>
+      {currentUser ? (
+        <button className='button logout' onClick={handleLogout}>
+          Logout
+        </button>
+      ) : (
+        <div className='buttons'>
+          <button className='button login' onClick={() => setShowLogin(true)}>
+            Login
+          </button>
+          <div className='divider' />
+          <button
+            className='button register'
+            onClick={() => setShowRegister(true)}
           >
-          <div className='popup'>
-            <Info pin={pin}/>
-          </div>
-        </Popup>
-    )}
-        </>
+            Register
+          </button>
+        </div>
+      )}
+      {showRegister && <Register setShowRegister={setShowRegister} />}
+      {showLogin && (
+        <Login
+          setShowLogin={setShowLogin}
+          myStorage={myStorage}
+          setCurrentUser={setCurrentUser}
+        />
+      )}
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+        mapStyle='mapbox://styles/sebastiangreen/ckrnp8ur54xux17mswwup4dhk'
+        onDblClick={handlePinClick}
+      >
+        {pins.map((pin) => (
+          <>
+            <Marker
+              latitude={pin.latitude}
+              longitude={pin.longitude}
+              offsetLeft={-viewport.zoom * 4.25}
+              offsetTop={-viewport.zoom * 8.5}
+            >
+              <Room
+                style={{
+                  fontSize: viewport.zoom * 8.5,
+                  color:
+                    pin.username === currentUser ? 'lightcoral' : '#8b95c9',
+                  cursor: 'pointer'
+                }}
+                onClick={() =>
+                  handlePopupClick(pin._id, pin.latitude, pin.longitude)
+                }
+              />
+            </Marker>
+            {pin._id === currentPinId && (
+              <Popup
+                latitude={pin.latitude}
+                longitude={pin.longitude}
+                closeButton={true}
+                closeOnClick={false}
+                anchor='bottom'
+                onClose={() => setCurrentPinId(null)}
+              >
+                <div className='popup'>
+                  <Info pin={pin} />
+                </div>
+              </Popup>
+            )}
+          </>
         ))}
         {newPin && (
-        <Popup
-          latitude={newPin.latitude}
-          longitude={newPin.longitude}
-          closeButton={true}
-          closeOnClick={false}
-          anchor="bottom"
-          onClose={()=>setNewPin(null)}
+          <Popup
+            latitude={newPin.latitude}
+            longitude={newPin.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            anchor='bottom'
+            onClose={() => setNewPin(null)}
           >
             <div>
               <form onSubmit={handleSubmit}>
                 <label>Location</label>
-                <input placeholder='Enter a location' onChange={(e)=>setLocation(e.target.value)}/>
+                <input
+                  placeholder='Enter a location'
+                  onChange={(e) => setLocation(e.target.value)}
+                />
                 <label>Movie</label>
-                <textarea placeholder='This place appeared in...' onChange={(e)=>setMovie(e.target.value)}/>
+                <textarea
+                  placeholder='This place appeared in...'
+                  onChange={(e) => setMovie(e.target.value)}
+                />
                 <label>Rating</label>
-                <select onChange={(e)=>setRating(e.target.value)}>
+                <select onChange={(e) => setRating(e.target.value)}>
                   <option value='1'>1</option>
                   <option value='2'>2</option>
                   <option value='3'>3</option>
                   <option value='4'>4</option>
                   <option value='5'>5</option>
                 </select>
-                <button className='submitButton' type='submit'>Add Pin</button>
+                <button className='submitButton' type='submit'>
+                  Add Pin
+                </button>
               </form>
             </div>
-        </Popup>
+          </Popup>
         )}
       </ReactMapGL>
     </div>
--- server/index.js
+++ server/index.js
@@ -4,28 +4,30 @@ const mongoose = require('mongoose');
 const pinRoute = require('./routes/pin');
 const userRoute = require('./routes/users');
 require('dotenv').config();
+
 const PORT = process.env.PORT;
 const DB_URL = process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(`${DB_URL}${DB_NAME}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose
+  .connect(`${DB_URL}${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
-    console.log('Database is connected!📍')
+    console.log('Database is connected!📍');
   })
   .catch((e) => console.log(e));
 
 app.use('/routes/pins', pinRoute);
 app.use('/routes/users', userRoute);
 
-app.listen(PORT, (e) => {
+app.listen(process.env.PORT, (e) => {
   if (e) console.log(e);
-  console.log(`Server is running on http://localhost:${PORT}!!🚀`)
-})
-
+  console.log(`Server is running on http://localhost:${PORT}!!🚀`);
+});
